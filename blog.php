dc3504169ad1396f6ec59269f8693baec52bc7a2
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-09-27 13:18:43 +0100

    Adding new page template for blog

---
+++ blog.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ralph Rutter</title>
+    <link rel="stylesheet" type="text/css" href="css/global.css">
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/top-bar.css">
+    <link rel="stylesheet" type="text/css" href="css/content.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/typography.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+<body>
+<div id="top-bar">
+<!--    <img src="images/mathematics-989124_1920.jpg" alt="mathematical diagram montage">-->
+</div>
+<header>
+    <a class="nav-button previous" href="about.html">
+        <img src="images/about-icon.png" alt="about icon">
+    </a>
+    <div id="main-heading">
+        <h1>Ralph Rutter</h1>
+    </div>
+    <a class="nav-button next" href="portfolio.html">
+        <img src="images/portfolio-icon.png" alt="portfolio icon">
+    </a>
+</header>
+<main>
+    <h2 class="subheading">Junior Full-Stack Developer</h2>
+</main>
+<nav>
+    <a class="big-button" href="contact.html">Get In Touch</a>
+    <div id="nav-bar">
+        <a class="button" href="about.html">
+            <div class="icon-container">
+                <div class="icon">
+                </div>
+            </div>
+            <div>
+                About Me
+            </div>
+        </a>
+        <a class="button current" href="index.html">
+            <div class="icon-container">
+                <div class="icon">
+                </div>
+            </div>
+            <div>
+                Home
+            </div>
+        </a>
+        <a class="button" href="portfolio.html">
+            <div class="icon-container">
+                <div class="icon">
+                </div>
+            </div>
+            <div>
+                Portfolio
+            </div>
+        </a>
+    </div>
+</nav>
+<footer>
+    Icons made by Freepik from www.flaticon.com
+</footer>
+</body>
+</html>
