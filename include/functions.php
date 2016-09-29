0461f748e043b1f7ea11868d8f3590b6d9441f24
Ralph Rutter <ralph.rutter@yahoo.co.uk>
2016-09-29 23:22:47 +0100

    Added functions for the blog archive page

--- include/functions.php
+++ include/functions.php
@@ -48,4 +48,50 @@ function links_most_recent($blog_posts) {
     return $links;
 }
 
+
+/**
+ * Makes an mysqli query to get id, article title, short description, date and GET query string for all blog posts
+ *
+ * @param $con OBJECT the value returned by the database connection script
+ * @return ARRAY an indexed array containing associative arrays representing table rows.
+ */
+function query_archive($con) {
+    // Request data from database
+    $result = mysqli_query($con, "
+    SELECT `id`, `name`, `desc`, `date_created`, `slug`
+    FROM `articles`
+    ORDER BY `date_created` DESC    
+    "
+    );
+
+    // Put data into an indexed array, each element of which is an assoc. array representing a row of the table
+    $result_archive = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    return $result_archive;
+}
+
+/**
+ * Takes an array of blog posts and puts out an html string containing a link for each one, containing the title, date
+ * and short description for each one.
+ *
+ * @param $blog_posts ARRAY an indexed array, each element of which is an assoc. array representing a row of the table
+ * (one post).
+ * @return STRING a collection of anchor tags for all the previous posts, to go on the archive page
+ */
+function links_archive($blog_posts) {
+    $links = '';
+    foreach ($blog_posts as $post) {
+        $links .= "
+<a class='large-list-item' href=\"blog.php?page=article&title=" . $post['slug'] . "\">
+    <div class=\"column1\">" . $post['name'] . "
+    </div>
+    <div class=\"column2\">" . $post['date_created'] . "
+    </div>
+    <div class=\"column3\">" . $post['desc'] . "
+    </div>
+</a>
+";
+    }
+    return $links;
+}
 ?>
